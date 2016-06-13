520965e7dc5c37b1bad2c7de8877977fe7bd20a0
Mikhail <misha.stepanov@mail.ru>
2016-06-14 00:31:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+'use strict';
+
 var urlModule = require('url');
-function parse(arg){
-  if(Object.prototype.toString.call(arg).slice(8, -1) === 'String') return urlModule.parse(arg);
-  if(Object.prototype.toString.call(arg).slice(8, -1) === 'Object') return urlModule.format(arg);
-  else return new Error('This is neither object or string');
+function parse(arg) {
+  if (Object.prototype.toString.call(arg).slice(8, -1) === 'String') return urlModule.parse(arg);
+  if (Object.prototype.toString.call(arg).slice(8, -1) === 'Object') return urlModule.format(arg);else return new Error('This is neither object or string');
 }
-module.exports= parse;
+module.exports = parse;
